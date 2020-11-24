2738adcf0fe8b3edca038b75682a69908c91ab54
lyndseawar <58059165+lyndseawar@users.noreply.github.com>
2020-11-24 12:30:26 -0500

    Add files via upload

---
+++ name.py
@@ -0,0 +1,12 @@
+"""
+Name: Lyndsea Warkenthien 
+Assingment: This is the second chapter of the Python Crash Course Book 
+Details: Changing Case in a String with Methods
+A method is an action that Python can preform on a piece of data. The dot (.)
+after name in name.title() tells python to make the title() method act on the 
+variable name. 
+
+"""
+
+name = "ada lovelace"
+print(name.title())
